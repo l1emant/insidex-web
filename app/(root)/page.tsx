6ec0db0025bca219c75f7d28b97aa11c49c6e709
l1emant <91789840+l1emant@users.noreply.github.com>
2025-09-29 23:24:30 +0530

    Implement Header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import {JSX} from "react";
+
+const Home: () => JSX.Element = () => {
+    return (
+        <div className="flex min-h-screen home-wrapper">
+            Home
+        </div>
+    )
+}
+export default Home
